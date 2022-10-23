8dbd34c6d0b0b2af00e84d4ea80f65ec7096e568
Rubén Comerón Galán <ruben.comeron@edu.uah.es>
2022-10-24 00:01:32 +0200

    Guarda datos cada 2 min pero no se refleja en la página

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import requests, re, pymongo, time, datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -6,13 +7,21 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["servidor"]
 mycol = mydb["euro"]
 
-@app.route("/")
-def index():
+valor = 0.0
+
+def recoger_valor():
     r = requests.get("http://es.investing.com/currencies/")
     valor = float((str(re.findall("pid-1-last..(\d,\d{4})", r.text))[2:-2]).replace(",", "."))
 
     mydict = {"value": valor, "time": datetime.datetime.now()}
     x = mycol.insert_one(mydict)
+    
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(recoger_valor, 'interval', minutes=2)
+sched.start()
+
+@app.route("/")
+def index():
     return render_template('index.html', value=valor)
 
 if __name__ == "__main__":
