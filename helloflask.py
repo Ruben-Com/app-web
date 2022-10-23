ed0c38895ffea992fabbd28511b07aaf4f039269
Rubén <ruben.comeron@edu.uah.es>
2022-10-23 14:35:25 +0200

    Obtiene el valor correctamente

--- helloflask.py
+++ helloflask.py
@@ -1,10 +1,12 @@
-from flask import Flask
+import requests, re
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    r = requests.get("http://es.investing.com/currencies/")
+    valor = str(re.findall("pid-1-last..(\d,\d{4})", r.text))[2:-2]
+    return render_template('index.html', value=valor)
 
 if __name__ == "__main__":
-    #app.run()
     app.run(host='0.0.0.0', port=5000, debug=True)
