c1876a42a7d3bd7308ba50519132096f974e87e0
Rubén Comerón Galán <ruben.comeron@edu.uah.es>
2022-10-24 22:05:27 +0200

    DB con temporizador y página inicial

--- program.py
+++ program.py
@@ -7,12 +7,10 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["servidor"]
 mycol = mydb["euro"]
 
-valor = 0.0
 
 def recoger_valor():
     r = requests.get("http://es.investing.com/currencies/")
     valor = float((str(re.findall("pid-1-last..(\d,\d{4})", r.text))[2:-2]).replace(",", "."))
-
     mydict = {"value": valor, "time": datetime.datetime.now()}
     x = mycol.insert_one(mydict)
     
@@ -22,7 +20,11 @@ sched.start()
 
 @app.route("/")
 def index():
+    r = requests.get("http://es.investing.com/currencies/")
+    valor = float((str(re.findall("pid-1-last..(\d,\d{4})", r.text))[2:-2]).replace(",", "."))
+    mydict = {"value": valor, "time": datetime.datetime.now()}
+    x = mycol.insert_one(mydict)
     return render_template('index.html', value=valor)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
